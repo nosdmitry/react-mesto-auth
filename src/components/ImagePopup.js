92b760d42d5e2b680ab9787049997a966b9d283c
Dmitry Nosov <mizai@yandex.ru>
2021-03-08 22:46:32 +0300

    Bugs: imagePopup missed atr

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup(props) {
         <figure className="galery__popup-image-container">
           <button onClick={ props.onClose } type="button" aria-label="Close_popup" className="popup__exit-button galery__popup-exit"></button>
           <img src={ props.card.link } alt={ props.card.name } className="galery__fulsize-img" />
-          <figcaption className="galery__popup-text"></figcaption>
+          <figcaption className="galery__popup-text">{ props.card.name }</figcaption>
         </figure>
       </div>
     );
