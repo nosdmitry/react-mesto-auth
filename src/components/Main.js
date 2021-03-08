f78d75aa0adedc38c30e6dc54ee167a0ac0092ef
Dmitry Nosov <mizai@yandex.ru>
2021-03-08 11:19:52 +0300

    Refactor: state change

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,40 +3,46 @@ import api from './Api';
 
 function Main(props) {  
 
-  //let userName = 'Иван Иванович';
-  let userDescription = 'Птица-курица';
-  let userAvatar = 'https://i1.wallbox.ru/wallpapers/main/201507/e813ea0dce719bd.jpg';
-
-  const [userData, setUserData] = React.useState({});
+  const [userName, setUserName] = React.useState('Загрузка');
+  const [userDescription, setUserDescription] = React.useState('Загрузка');
+  const [userAvatar, setUserAvatar] = React.useState('./static/media/avatar-loader.7741db8b.gif');
+  
   React.useEffect(() => {
     api.getUserInfo()
       .then(data => {
-        setUserData(data);
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
       })
       .catch(err => console.log('#######ERROR!!', err));
-  }, []);
-
-
-  console.log('###### name: ', userData.name)
+  }, []);  
 
   return (
     <main>
       <section className="profile">
-        {/* <button onClick={handleEditAvatarClick} className="profile__image profile__image_visible_hidden" aria-label="Аватар"></button> */}
         <button onClick={props.onEditAvatar}
           className="profile__image profile__image" 
-          style={{ backgroundImage: `url(${userData.avatar})`}}
+          style={{ backgroundImage: `url(${userAvatar})`}}
           aria-label="Аватар">
         </button>
-        {/* <div className="profile__image profile__image_loading"></div> */}
         <div className="profile__text-wrap">
           <div className="profile__title-wrap">
-            <h1 className="profile__title" aria-label="Имя">{userData.name}</h1>
-            <button onClick={props.onEditProfile} type="button" aria-label="Edit_profile" className="profile__edit"></button>
+            <h1 className="profile__title" aria-label="Имя">{userName}</h1>
+            <button 
+              onClick={props.onEditProfile}
+              type="button" 
+              aria-label="Edit_profile" 
+              className="profile__edit">
+            </button>
           </div>
-          <p className="profile__subtitle" aria-label="Описание">{userData.about}</p>
+          <p className="profile__subtitle" aria-label="Описание">{userDescription}</p>
         </div>
-        <button onClick={props.onAddPlace} type="button" aria-label="Add_img" className="profile__add-card-button"></button>
+        <button 
+          onClick={props.onAddPlace} 
+          type="button" 
+          aria-label="Add_img" 
+          className="profile__add-card-button">
+        </button>
       </section>
 
       <section className="galery galery_margins" aria-label="Галерея">
