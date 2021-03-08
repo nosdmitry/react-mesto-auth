8c0be34ba6d640002f1eda343ec3bb865193ceab
Dmitry Nosov <mizai@yandex.ru>
2021-03-08 10:26:19 +0300

    Feature: get user data from server

---
+++ src/components/Api.js
@@ -0,0 +1,97 @@
+export class Api {
+  constructor(options) {
+    this._url = options.url;
+    this._headers = options.headers;
+    this._body = options.body;
+  }
+
+  _onError(res, message) {
+    if(res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`${message}: ` + res.status);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._url}users/me`, {
+      headers: this._headers
+    })
+    .then(res => this._onError(res, 'Ошибка при обращении к серверу'))
+  }
+
+  editUserInfo(data) {
+    return fetch(`${this._url}users/me`, {
+      method: 'PATCH',
+      headers: {
+        authorization: this._headers.authorization,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+      })
+      .then(res => this._onError(res, 'Ошибка при редактировании данных пользователя'))
+  }
+
+  getAllCards() {
+    return fetch(`${this._url}cards/`, {
+        headers: this._headers
+      })
+      .then(res => this._onError(res, 'Ошибка при получении данных карточек'))
+  }
+
+  addNewCard(data) {
+    return fetch(`${this._url}cards/`, {
+      method: 'POST',
+      headers: {
+        authorization: this._headers.authorization,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => this._onError(res, 'Ошибка при добавлении новой карточки'))
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this._url}cards/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res => this._onError(res, 'Ошибка при удалении картчоки'))
+  }
+
+  addLike(cardId) {
+    return fetch(`${this._url}cards/likes/${cardId}`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+    .then(res => this._onError(res, 'Ошибка при обработке лайка'))
+  }
+
+  removeLike(cardId) {
+    return fetch(`${this._url}cards/likes/${cardId}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+    .then(res => this._onError(res, 'Ошибка при обработке лайка'))
+  }
+
+  changeAvatar(data) {
+    return fetch('https://mesto.nomoreparties.co/v1/cohort-20/users/me/avatar', {
+      method: 'PATCH',
+      headers: {
+        authorization: this._headers.authorization,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => this._onError(res, 'Не удалось изменить аватарку'))
+  }
+}
+
+const api = new Api({
+  url: 'https://mesto.nomoreparties.co/v1/cohort-20/',
+  headers: {
+    authorization: '036c4f02-47a4-4c62-a975-bbce507f165f'
+  }
+})
+
+export default api;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,40 @@
+import React from 'react';
+import api from './Api';
+
 function Main(props) {  
+
+  //let userName = 'Иван Иванович';
+  let userDescription = 'Птица-курица';
+  let userAvatar = 'https://i1.wallbox.ru/wallpapers/main/201507/e813ea0dce719bd.jpg';
+
+  const [userData, setUserData] = React.useState({});
+  React.useEffect(() => {
+    api.getUserInfo()
+      .then(data => {
+        setUserData(data);
+      })
+      .catch(err => console.log('#######ERROR!!', err));
+  }, []);
+
+
+  console.log('###### name: ', userData.name)
 
   return (
     <main>
       <section className="profile">
         {/* <button onClick={handleEditAvatarClick} className="profile__image profile__image_visible_hidden" aria-label="Аватар"></button> */}
-        <button onClick={props.onEditAvatar} className="profile__image profile__image" aria-label="Аватар"></button>
-        <div className="profile__image profile__image_loading"></div>
+        <button onClick={props.onEditAvatar}
+          className="profile__image profile__image" 
+          style={{ backgroundImage: `url(${userData.avatar})`}}
+          aria-label="Аватар">
+        </button>
+        {/* <div className="profile__image profile__image_loading"></div> */}
         <div className="profile__text-wrap">
           <div className="profile__title-wrap">
-            <h1 className="profile__title" aria-label="Имя">Имя</h1>
+            <h1 className="profile__title" aria-label="Имя">{userData.name}</h1>
             <button onClick={props.onEditProfile} type="button" aria-label="Edit_profile" className="profile__edit"></button>
           </div>
-          <p className="profile__subtitle" aria-label="Описание"></p>
+          <p className="profile__subtitle" aria-label="Описание">{userData.about}</p>
         </div>
         <button onClick={props.onAddPlace} type="button" aria-label="Add_img" className="profile__add-card-button"></button>
       </section>
